03e18546ed9504885e2bdcd7733b4c7fbb241664
Artyom Gornostayev <temofey1989@gmail.com>
2025-03-11 18:43:08 +0100

    chore: Version Catalog support

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "2.1.10"
-    id("org.jmailen.kotlinter") version "5.0.1"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlinter)
 }
 
 repositories {
@@ -15,46 +15,15 @@ repositories {
     maven { url = uri("https://jitpack.io") }
 }
 
-val emojiVersion: String by project
-val jacksonVersion: String by project
-val kotestVersion: String by project
-val kotlinBoostVersion: String by project
-val kotlinCoroutinesVersion: String by project
-val mockkVersion: String by project
-val retrofitVersion: String by project
-val telegramBotVersion: String by project
-
 dependencies {
-    api(platform("io.justdevit.kotlin:boost-bom:$kotlinBoostVersion"))
-
-    // Kotlin
-    implementation(kotlin("stdlib"))
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:$kotlinCoroutinesVersion")
-
-    // Logging
-    api("io.justdevit.kotlin:boost-logging-slf4j")
-    api("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j")
-
-    // Serialization
-    api("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
-    api("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:$jacksonVersion")
-    api("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
-
-    // Other
-    api("com.squareup.retrofit2:retrofit:$retrofitVersion") {
+    api(platform(libs.boost.bom))
+    api(libs.bundles.kotlin.coroutines)
+    api(libs.bundles.logging)
+    api(libs.bundles.serialization)
+    api(libs.bundles.other) {
         exclude(group = "com.squareup.okhttp3", module = "okhttp")
     }
-    api("org.kodein.emoji:emoji-kt:$emojiVersion")
-    api("io.justdevit.kotlin:boost-commons")
-    api("io.justdevit.kotlin:boost-eventbus")
-    api("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:$telegramBotVersion")
-
-    // Testing
-    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
-    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
-    testImplementation("io.kotest:kotest-property:$kotestVersion")
-    testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation(libs.bundles.testing)
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_21
@@ -65,6 +34,12 @@ java {
 }
 
 tasks {
+    formatKotlin {
+    }
+
+    lintKotlin {
+    }
+
     withType<KotlinCompile> {
         compilerOptions {
             freeCompilerArgs = listOf(
